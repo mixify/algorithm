a1d68b3eed4bcf570da144adafa4c8dbec30c235
mixify <mixify@naver.com>
2019-01-02 16:30:37 +0900

    fugitive testing~~~

--- boggle.cpp
+++ boggle.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
+
+void solve_problem(int case_num);
 int main(int argc, char *argv[])
 {
-
+    int cases;
+    cin>>cases;
+    for (int i = 0; i < cases; ++i) {
+        solve_problem(i);
+    }
     return 0;
+}
+void solve_problem(int case_num)
+{
+    int word_count;
+    cin>>word_count;
 }
