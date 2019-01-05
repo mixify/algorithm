574641716e7a9638070f394e76d2d0897d92361a
mixify <mixify@github.com>
2019-01-05 21:17:51 +0900

    quadtree algorithm solve

---
+++ quadtree.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+void solve_problem(int case_num);
+int main(int argc, char *argv[])
+{
+    int cases;
+    cin>>cases;
+    for (int i = 0; i < cases; ++i) {
+        solve_problem(i);
+    }
+    return 0;
+}
+
+void solve_problem(int case_num)
+{
+    string quadtree;
+    getline(cin,quadtree);
+    for (int i = 0; i < quadtree.length(); ++i) {
+
+    }
+}
