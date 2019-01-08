e407d26538d4e1a597c5900abef2533f01978d0f
mixify <mixify@github.com>
2019-01-08 22:30:21 +0900

    paper algorithm solve

--- paper.cpp
+++ paper.cpp
@@ -5,15 +5,14 @@
 #include <utility>
 
 using namespace std;
-
-void cut_paper(int x, int y, int size, vector<vector <int> > paper);
+vector<vector <int> > paper;
+void cut_paper(int x, int y, int size);
 int minus_count;
 int zero_count;
 int plus_count;
 int main(int argc, char *argv[])
 {
     int N;
-    vector<vector <int> > paper;
     scanf("%d", &N);
     minus_count = 0;
     zero_count = 0;
@@ -27,11 +26,11 @@ int main(int argc, char *argv[])
             paper[i].push_back(num);
         }
     }
-    cut_paper(0,0, N,paper);
+    cut_paper(0,0,N);
     printf("%d\n%d\n%d\n",minus_count, zero_count, plus_count);
 }
 
-void cut_paper(int x, int y, int size, vector<vector <int> > paper)
+void cut_paper(int x, int y, int size)
 {
     bool same = true;
     for (int i = x; i < x+size; ++i) {
@@ -60,7 +59,7 @@ void cut_paper(int x, int y, int size, vector<vector <int> > paper)
         size/=3;
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
-                cut_paper(x+size*i,y+size*j,size,paper);
+                cut_paper(x+size*i,y+size*j,size);
             }
         }
     }
