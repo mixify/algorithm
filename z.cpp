6067a36204988674d44c183c54972b8a9551f9d7
mixify <mixify@github.com>
2019-01-09 19:45:10 +0900

    z algorithm solve

---
+++ z.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <set>
+#include <map>
+
+using namespace std;
+
+void Z(int x,int y, int N);
+int row,col;
+int order = 0;
+int val = 0;
+int main(int argc, char *argv[])
+{
+    int N;
+    int size = 1;
+    cin>>N;
+    cin>>row>>col;
+    for (int i = 0; i < N; ++i) {
+        size*=2;
+    }
+    Z(0,0,size);
+    printf("%d\n", val) ;
+    return 0;
+}
+
+void Z(int x,int y, int N)
+{
+    if(N==1)
+    {
+        if(x==row && y==col)
+            val = order;
+        order++;
+        return;
+    }
+    N/=2;
+    Z(x,y,N);
+    Z(x,y+N,N);
+    Z(x+N,y,N);
+    Z(x+N,y+N,N);
+}
