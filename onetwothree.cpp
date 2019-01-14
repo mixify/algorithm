36ad75745f0652a0aea1e8c6b5b4c179e7991cd0
mixify <mixify@github.com>
2019-01-14 13:47:51 +0900

    onetwothree algorithm solve

--- onetwothree.cpp
+++ onetwothree.cpp
@@ -15,7 +15,11 @@ void solve_problem(int case_num);
 int onetwothree(int n, int one, int two, int three);
 int main(int argc, char *argv[])
 {
-    solve_problem(0);
+    int cases;
+    cin>>cases;
+    for (int i = 0; i < cases; ++i) {
+        solve_problem(i);
+    }
     return 0;
 }
 
@@ -32,7 +36,6 @@ int onetwothree(int n, int one, int two, int three)
     int &ret = cache[one][two][three];
     if(ret!=-1) return ret;
 
-    // printf("%d %d %d\n", one, two, three);
     if(one + 2*two + 3*three == n)
         return 1;
     else if(one + 2*two + 3*three > n)
