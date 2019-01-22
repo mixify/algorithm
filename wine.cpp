a5b6cfc4b08b094a654631da45821e2c176fa081
mixify <mixify@github.com>
2019-01-22 15:29:52 +0900

    wine algorithm solve comment :

--- wine.cpp
+++ wine.cpp
@@ -19,12 +19,14 @@ int solve_problem(int idx,int cnt)
     if(cnt>=3) return -1;
     if(idx>=N) return 0;
     if(idx==N-1) return wine[idx];
+
+
+    if(idx<0)
+        return max(max(solve_problem(idx+1,1), solve_problem(idx+2,1)),solve_problem(idx+3,1));
+
     int &ret = cache[idx][cnt];
     if(ret!=-1) return ret;
     // ret = 0;
-    if(idx<0)
-        return ret = max(max(solve_problem(idx+1,cnt+1), solve_problem(idx+2,1)),solve_problem(idx+3,1));
-    else
         return ret = wine[idx] + max(max(solve_problem(idx+1,cnt+1), solve_problem(idx+2,1)),solve_problem(idx+3,1));
 }
 int main(int argc, char *argv[])
