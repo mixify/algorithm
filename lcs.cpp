54e03233b744fefb1028b499c29bbd7494c2387b
mixify <mixify@github.com>
2019-01-23 23:07:31 +0900

    lcs algorithm solve comment :

--- lcs.cpp
+++ lcs.cpp
@@ -11,9 +11,8 @@
 using namespace std;
 
 int N,M;
-const long long NEGINF = numeric_limits<long long>::min();
 int cache[1001][1001];
-int solve(string &seq1,string &seq2, int idx1, int idx2, int last_elem);
+int solve(string &seq1,string &seq2, int idx1, int idx2);
 void solve_problem(int case_num);
 int main(int argc, char *argv[])
 {
@@ -32,32 +31,33 @@ void solve_problem(int case_num)
 
     memset(cache, -1, sizeof(cache));
     // mx = max(solve(seq1,seq2,0,0), mx);
-    printf("%d\n", solve(seq1,seq2,-1,-1,0)/* -2 */);
+    printf("%d\n", solve(seq1,seq2,0,0)/* -2 */);
     // for (int i = 1; i < JLIS.size(); ++i) {
     //     printf("%d ", JLIS[i]);
     // }
     // printf("\n");
 }
-int solve(string &seq1,string &seq2, int idx1, int idx2, int last_elem)
+int solve(string &seq1,string &seq2, int idx1, int idx2)
 {
-    int &ret = cache[idx1+1][idx2+1];
+    int &ret = cache[idx1][idx2];
     if(ret != -1)
         return ret;
 
     ret = 0;
-    bool first = false;
-    if(last_elem == 0)
-        first = true;
+    for (int i = idx1; i < N; ++i)
+    {
+        for (int j = idx2; j < M; ++j)
+            if (seq1[i] == seq2[j])
+                ret = max(ret, solve(seq1,seq2,i+1,j+1)+1);
+    }
+    // ret = max(ret, solve(seq1,seq2,i,idx2,seq1[i])+1);
+    // bool first = false;
+    // if(last_elem == 0)
+    //     first = true;
     // ret = 2;
     // long long a = (idx1==-1 ? NEGINF : seq1[idx1]);
     // long long b = (idx2==-1 ? NEGINF : seq2[idx2]);
     // long long last_elem = max(a,b);
     // printf("%lld\n", last_elem);
-    for (int i = idx1+1; i < N; ++i)
-        if(first||last_elem == seq1[i])
-            ret = max(ret, solve(seq1,seq2,i,idx2,seq1[i])+1);
-    for (int j = idx2+1; j < M; ++j)
-        if(first||last_elem == seq2[j])
-            ret = max(ret, solve(seq1,seq2,idx1,j,seq2[j])+1);
     return ret;
 }
