2a1b92d90ff36f6d4edc4c578845c5794d2fdb25
mixify <mixify@github.com>
2019-01-23 21:47:22 +0900

    coin1 algorithm solve comment :

--- coin1.cpp
+++ coin1.cpp
@@ -11,33 +11,46 @@
 
 using namespace std;
 int N, M;
-vector<int> coin;
 
-int cache[10000][100];
-int solve(int current,int prev)
-{
-    int &ret = cache[current][prev];
-    if(ret!=-1) return ret;
-    if(current==M) return ret = 1;
-    if(current>M) return ret = 0;
-    ret=0;
-    for (int i = 0; i < coin.size(); ++i)
-    {
-        if(coin[i]>=coin[prev])
-            ret+=solve(current+coin[i],i);
-    }
-    return ret;
-}
+int cache[10001];//4MB
+// int solve(int current,int prev)
+// {
+//     int &ret = cache[current];// [prev];
+//     if(current==M) return ret = 1;
+//     if(current + coin[prev] > M) return ret = 0;
+//     // if(ret!=-1)
+//     //     return ret;
+//     ret=0;
+//     for (int i = prev; i < N; ++i)
+//         ret+=solve(current+coin[i],i);//중복되는 것 처리 해야함..
+//     return ret;
+// }
 
 int main(int argc, char *argv[])
 {
     int cases;
     cin>>N>>M;
+    memset(cache, 0, sizeof(cache));
     for (int i = 0; i < N; ++i) {
-        int in; cin>>in; coin.push_back(in);
+        int in; cin>>in;
+        if(i==0)
+            for (int j = 0; j <= M; j+=in)
+                cache[j]++;
+        else
+        {
+            for (int j = 0; j <= M; j++)
+            {
+                if(j-in>=0)
+                    cache[j] = cache[j] + cache[j-in];
+            }
+        }
     }
-    memset(cache, -1, sizeof(cache));
-    printf("%d\n", solve(0,0));
+    // for (int j = 0; j <= M; j++)
+    // {
+    //     printf("%d ", cache[j]);
+    // }
+    // printf("\n");
+    printf("%d\n", cache[M]);
     return 0;
 }
 
