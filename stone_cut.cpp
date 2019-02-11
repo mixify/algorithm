e1f6f8229fbec3aaaf4e0553671df452f20461ea
mixify <mixify@github.com>
2019-02-11 17:05:20 +0900

    stone_cut algorithm solve comment :

--- stone_cut.cpp
+++ stone_cut.cpp
@@ -20,9 +20,9 @@ vector<vector<int> > stone;
 bool dividable(int x, int y, int slice_orientation,vector<pair<int,int> > crystal)
 {
     for (int i = 0; i < crystal.size(); ++i) {
-        if(slice_orientation==VERTICAL && crystal[i].first == x)
+        if(slice_orientation==VERTICAL && crystal[i].second == x)
             return false;
-        if(slice_orientation==HORIZONTAL && crystal[i].second == y)
+        if(slice_orientation==HORIZONTAL && crystal[i].first == y)
             return false;
     }
     return true;
@@ -31,12 +31,12 @@ int cut_stone(int x, int y, int size_x, int size_y, int slice_orientation)
 {
     vector<pair<int,int> > dirty;
     vector<pair<int,int> > crystal;
-    for (int i = x; i < size_x; ++i) {
-        for (int j = y; j < size_y; ++j) {
-            if(stone[i][j] == 1)
-                dirty.push_back(make_pair(i,j));
-            else if(stone[i][j] == 2)
-                crystal.push_back(make_pair(i,j));
+    for (int i = 0; i < size_x; ++i) {
+        for (int j = 0; j < size_y; ++j) {
+            if(stone[y+j][x+i] == 1)
+                dirty.push_back(make_pair(j,i));
+            else if(stone[y+j][x+i] == 2)
+                crystal.push_back(make_pair(j,i));
         }
     }
 
@@ -50,34 +50,48 @@ int cut_stone(int x, int y, int size_x, int size_y, int slice_orientation)
             return 0;
     }
 
+    if(dirty.size()==0)
+    {
+        return 0;
+    }
+    int ret = 0;
     for (int i = 0; i < dirty.size(); ++i)
     {
-        int divided_x = dirty[i].first;
-        int divided_y = dirty[i].second;
+        int divided_y = dirty[i].first;
+        int divided_x = dirty[i].second;
         if(slice_orientation==UNKNOWN)
         {
             if(dividable(divided_x,divided_y,VERTICAL,crystal))
             {
-                cut_stone(x,y,divided_x-x,y,VERTICAL);
-                cut_stone(divided_x+1,y,size_x + x-divided_x - 1,y,VERTICAL);
+                ret += cut_stone(x,y,divided_x-x,size_y,VERTICAL) && cut_stone(divided_x+1,y,size_x + x-divided_x - 1,size_y,VERTICAL);
             }
             if(dividable(divided_x,divided_y,HORIZONTAL,crystal))
             {
-                cut_stone(x,y,x,divided_y-y,HORIZONTAL);
-                cut_stone(x,divided_y+1,x,size_y + y-divided_y -1,HORIZONTAL);
+                ret += cut_stone(x,y,size_x,divided_y-y,HORIZONTAL) && cut_stone(x,divided_y+1,size_x,size_y + y-divided_y -1,HORIZONTAL);
             }
         }
         else if(slice_orientation == HORIZONTAL)
         {
-            cut_stone(x,y,divided_x-x,y,VERTICAL);
-            cut_stone(divided_x+1,y,size_x + x-divided_x - 1,y,VERTICAL);
+            if(dividable(divided_x,divided_y,VERTICAL,crystal))
+            {
+                if(!(cut_stone(x,y,divided_x-x,size_y,VERTICAL) && cut_stone(divided_x+1,y,size_x + x-divided_x - 1,size_y,VERTICAL)))
+                    return 0;
+            }
         }
         else
         {
-            cut_stone(x,y,x,divided_y-y,HORIZONTAL);
-            cut_stone(x,divided_y+1,x,size_y + y-divided_y -1,HORIZONTAL);
+            if(dividable(divided_x,divided_y,HORIZONTAL,crystal))
+            {
+                if(!(cut_stone(x,y,size_x,divided_y-y,HORIZONTAL) && cut_stone(x,divided_y+1,size_x,size_y + y-divided_y -1,HORIZONTAL)))
+                    return 0;
+            }
         }
     }
+
+    if(slice_orientation==UNKNOWN)
+        return ret;
+    else
+        return 1;
 }
 int main(int argc, char *argv[])
 {
