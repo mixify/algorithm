7f6b149324528385ec52bb9e3d120c2b7f0b80f6
mixify <mixify@github.com>
2019-03-15 19:47:36 +0900

    natural_num.cpp algorithm solve comment :

--- natural_num.cpp
+++ natural_num.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int N;
 vector<int> number;
-
-// int cache[20001];
-
-// int best_natural(int x, int y)
-// {
-//
-// }
 int main(int argc, char *argv[])
 {
+    cin>>N;
     for (int i = 0; i < N; ++i) {
         int in; cin>>in; number.push_back(in);
     }
 
+    sort(number.begin(),number.end());
     printf("%d\n", N%2==0?number[N/2-1]:number[N/2]);
     // memset(cache,-1,sizeof(cache));
     // best_natural(0,N-1);
