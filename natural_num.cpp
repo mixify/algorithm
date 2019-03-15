9d5ba8c265f2ba51bcb19be94a60c3f8734be8b1
mixify <mixify@github.com>
2019-03-15 19:46:14 +0900

    natural_num.cpp algorithm solve comment :

--- natural_num.cpp
+++ natural_num.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int N;
-int min_n,max_n;
 vector<int> number;
 
 // int cache[20001];
@@ -14,25 +13,11 @@ vector<int> number;
 // }
 int main(int argc, char *argv[])
 {
-    cin>>N;
-    min_n = 20000; max_n = 0;
     for (int i = 0; i < N; ++i) {
         int in; cin>>in; number.push_back(in);
-        if(in<min_n) min_n = in;
-        if(in>max_n) max_n = in;
     }
-    int best = 99999999;
-    int best_num = 0;
-    for (int i = min_n; i <= max_n; ++i) {
-        int sum = 0;
-        for (int j = 0; j < N; ++j) sum+=abs(number[j]-i);
-        if(sum<best)
-        {
-            best = sum;
-            best_num = i;
-        }
-    }
-    printf("%d\n", best_num);
+
+    printf("%d\n", N%2==0?number[N/2-1]:number[N/2]);
     // memset(cache,-1,sizeof(cache));
     // best_natural(0,N-1);
 
