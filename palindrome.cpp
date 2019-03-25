a072ce513e4bf7ea4498bfef907d11ac4c744d46
mixify <mixify@github.com>
2019-03-25 21:59:06 +0900

    palindrome.cpp algorithm solve comment :

--- palindrome.cpp
+++ palindrome.cpp
@@ -12,19 +12,19 @@
 using namespace std;
 string str;
 int cache[2501][2501];// [2501];
-bool sym_cache[2501][2501];// [2501];
+int sym_cache[2501][2501];// [2501];
 
 bool is_symmetry(int i, int j)
 {
-    bool &ret = sym_cache[i][j];
+    int &ret = sym_cache[i][j];
     if(ret!=-1) return ret;
     while(i<j)
     {
         if(str[i] != str[j])
-            return ret = false;
+            return ret = 0;
         i++; j--;
     }
-    return ret = true;
+    return ret = 1;
 }
 int m = 2500;
 int palindrome(int idx,int count)
@@ -38,7 +38,8 @@ int palindrome(int idx,int count)
 
     ret = 0;
 
-    for (int i = idx; i < str.length(); ++i) {
+    for (int i = str.length()-1; i >= idx; --i) {
+        // if()
         if(is_symmetry(idx,i))
         {
             // printf("from %d to %d\n", idx,i);
@@ -46,6 +47,7 @@ int palindrome(int idx,int count)
             //how to ignore useless duplicates??
             int bef = ret;
             ret += palindrome(i+1,count+1);
+            // break;
         }
     }
     // printf("at %d ret = %d\n", idx, ret);
@@ -55,6 +57,7 @@ int main(int argc, char *argv[])
 {
     cin>>str;
     memset(cache,-1,sizeof(cache));
+    memset(sym_cache,-1,sizeof(sym_cache));
     palindrome(0,0);
     // printf("%d\n", palindrome(0,0));
     printf("%d\n", m);
