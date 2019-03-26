72937861a1b35f19ae8969189bd80958e080a1a2
mixify <mixify@github.com>
2019-03-26 14:23:30 +0900

    apnana.cpp algorithm solve comment :

--- apnana.cpp
+++ apnana.cpp
@@ -9,25 +9,27 @@ int land_A[1501][1501];//for lower
 int land_B[1501][1501];//for higher
 int summation_A[1501][1501];
 int summation_B[1501][1501];
-int cache[1501][1501];
+int cache[1501][1501][3];
 
 // int dy[3] = {1,0,1};
 // int dx[3] = {0,1,1};
-int bulldozer(int y, int x, int apple, int banana)
+int bulldozer(int y, int x,int ori, int apple, int banana)
 {
-    int &ret = cache[y][x];
-    // if(ret!=-1) return ret;
+    int &ret = cache[y][x][ori];
+    if(ret!=-1) {
+        return ret;
+    }
 
     // printf("%d %d => %d and %d\n", y,x,apple,banana);
     if(y==R-1 && x==C-1)
         return apple+banana;
     ret = 0;
     if(y<R-1 && x<C-1)
-        ret = max(bulldozer(y+1,x+1,apple+summation_A[y+1][x+1],banana+summation_B[y+1][x+1]), ret);
+        ret = max(bulldozer(y+1,x+1,2,apple+summation_A[y+1][x+1],banana+summation_B[y+1][x+1]), ret);
     if(y<R-1)
-        ret = max(bulldozer(y+1,x,apple-land_A[y+1][x],banana+summation_B[y+1][x]), ret);
+        ret = max(bulldozer(y+1,x,1,apple-land_A[y+1][x],banana+summation_B[y+1][x]), ret);
     if(x<C-1)
-        ret = max(bulldozer(y,x+1,apple+summation_A[y][x+1],banana-land_B[y][x+1]), ret);
+        ret = max(bulldozer(y,x+1,0,apple+summation_A[y][x+1],banana-land_B[y][x+1]), ret);
     return ret;
 }
 int main(int argc, char *argv[])
@@ -96,6 +98,6 @@ int main(int argc, char *argv[])
     //
     //     }
     // // }
-    printf("%d\n", bulldozer(0,0,summation_A[0][0],summation_B[0][0]));
+    printf("%d\n", bulldozer(0,0,0,summation_A[0][0],summation_B[0][0]));
     return 0;
 }
