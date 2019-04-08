29b294b51da790cd057de7cded76ac3eeb9f6ae4
mixify <mixify@github.com>
2019-04-08 14:29:29 +0900

    safe_place.cpp algorithm solve comment :

--- pop_transtion.cpp
+++ pop_transtion.cpp
@@ -5,6 +5,7 @@
 #include <climits>
 #include <cmath>
 #include <vector>
+#include <queue>
 #include <utility>
 #include <set>
 #include <map>
@@ -13,6 +14,9 @@ using namespace std;
 int N;
 int L,R;
 int country[51][51];
+int visited[51][51];
+int dy[4] = {-1,1,0,0};
+int dx[4] = {0,0,1,-1};
 
 int main(int argc, char *argv[])
 {
@@ -23,23 +27,71 @@ int main(int argc, char *argv[])
             cin>>country[i][j];
         }
     }
-    int open_count;
+    int total_count = 0;
+    bool happen;
     do{
-        open_count = 0;
-        for (int i = 0; i < N-1; ++i) {
-            for (int j = 0; j < N-1; ++j) {
-                if(abs(country[i][j] - country[i+1][j]) >= L && abs(country[i][j] - country[i+1][j]) <= R)
+        happen = false;
+        memset(visited,0,sizeof(visited));
+        queue<pair<int,int> > group;
+        // for (int i = 0; i < N; ++i)
+        // {
+        //     for (int j = 0; j < N; ++j)
+        //     {
+        //         printf("%d ", country[i][j]);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("----------------\n");
+        for (int i = 0; i < N; ++i)
+        {
+            for (int j = 0; j < N; ++j)
+            {
+                int open_count;
+                int sum;
+                vector<pair<int,int> > places;
+                open_count = 0;
+                if(!visited[i][j])
                 {
-                    open_count++;
+                    group.push(make_pair(i,j));
+                    places.push_back(make_pair(i,j));
+                    visited[i][j] = true;
+                    sum = country[i][j];
                 }
-                if(abs(country[i][j] - country[i][j+1]) >= L && abs(country[i][j] - country[i][j+1]) <= R)
+                while(!group.empty())
                 {
-                    open_count++;
+                    pair<int,int> yx = group.front();
+                    group.pop();
+                    for (int i = 0; i < 4; ++i) {
+                        int y = yx.first+dy[i];
+                        int x = yx.second+dx[i];
+                        if(y>=0 && y<N && x>=0 && x<N)
+                        {
+                            if(!visited[y][x])
+                            {
+                                int diff = abs(country[yx.first][yx.second] - country[y][x]);
+                                if(L <= diff && diff <= R)
+                                {
+                                    happen = true;
+                                    sum+=country[y][x];
+                                    places.push_back(make_pair(y,x));
+                                    open_count++;
+                                    visited[y][x] = true;
+                                    group.push(make_pair(y,x));
+                                }
+                            }
+                        }
+                    }
+                }
+                for (size_t k = 0; k < places.size(); ++k)
+                {
+                    country[places[k].first][places[k].second] = sum/places.size();
                 }
             }
         }
+        if(happen) total_count++;
 
-    }while(open_count > 0);
+    }while(happen);
+    printf("%d\n", total_count);
     return 0;
 }
 
