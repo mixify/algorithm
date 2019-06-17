2096a1376f13de4f659a943bdda6d2c85e1f52b5
mixify <mixify@naver.com>
2019-06-17 16:17:35 +0900

    time_machine.cpp algorithm solve comment :

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-a.out :network_connect.cpp 
-	g++ -g network_connect.cpp
-run : network_connect.cpp a.out
+a.out :time_machine.cpp 
+	g++ -g time_machine.cpp
+run : time_machine.cpp a.out
 	./a.out < input
-gdb : network_connect.cpp a.out
+gdb : time_machine.cpp a.out
 	gdb a.out
-algo : network_connect.cpp
-	algo network_connect
+algo : time_machine.cpp
+	algo time_machine
---
+++ time_machine.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <vector>
+#include <utility>
+#include <set>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int N, M;
+
+int INF = 999999999;
+
+map<int,vector<pair<int,int> > > grp;
+
+int main(int argc, char *argv[])
+{
+
+    cin>>N>>M;
+
+    for (int i = 0; i < M; ++i)
+    {
+        int from, to, val;
+        cin>>from>>to>>val;
+        grp[from].push_back(make_pair(val,to));
+    }
+
+    vector<int> visit = vector<int>(N+1,0);
+    vector<int> dis = vector<int>(N+1,INF);
+
+    queue<int> q;
+
+    q.push(1);
+    dis[1] = 0;
+
+    while(!q.empty())
+    {
+        int n = q.front();
+        q.pop();
+        visit[n]++;
+
+        if(visit[n] > N-1)
+        {
+            printf("-1\n");
+            return 0;
+        }
+        for(auto itr : grp[n])
+        {
+            pair<int,int> p = itr;
+            int d= p.first;
+            int to = p.second;
+            if(dis[n] + d < dis[to])
+                q.push(to);
+                dis[to] = dis[n] + d;
+        }
+    }
+    for (int i = 2; i <= N; ++i) {
+        printf("%d\n", dis[i]==INF?-1:dis[i]);
+    }
+    return 0;
+}
+
