9f3f051244ee6cf62e0894c1e6122473735579c0
mixify <mixify@naver.com>
2019-06-17 16:36:45 +0900

    time_machine.cpp algorithm solve comment :

--- time_machine.cpp
+++ time_machine.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     }
 
     vector<int> visit = vector<int>(N+1,0);
-    vector<int> dis = vector<int>(N+1,INF);
+    vector<long long> dis = vector<long long>(N+1,-1);
 
     deque<int> q;
 
@@ -52,20 +52,19 @@ int main(int argc, char *argv[])
         for(auto itr : grp[n])
         {
             pair<int,int> p = itr;
-            int d= p.first;
+            int d = p.first;
             int to = p.second;
-            if(dis[n] + d < dis[to])
+            if((dis[to]==-1) || (dis[n] + d < dis[to]))
+            {
                 dis[to] = dis[n] + d;
-            bool in = false;
-            for(auto it : q)
-                if(it == to)
-                    in = true;
-            if(!in)
-                q.push_back(to);
+
+                if(find(q.begin(), q.end(), to) == q.end())
+                    q.push_back(to);
+            }
         }
     }
     for (int i = 2; i <= N; ++i) {
-        printf("%d\n", dis[i]==INF?-1:dis[i]);
+        printf("%d\n", dis[i]);
     }
     return 0;
 }
